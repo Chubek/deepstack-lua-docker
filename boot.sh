d268d524639c73d7f5113dd6cbdc831de5d05a5d
Chubak <chubakbiddpaa@gmail.com>
2022-03-10 18:37:59 +0330

    testmatch?

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/DeepHoldem/ACPCServer && ./play_match.pl matchName holdem.limit.2p.reverse_blinds.game 1000 0 Alice ./example_player.limit.2p.sh Bob ./example_player.limit.2p.sh
+cd ~/DeepHoldem/ACPCServer && ./dealer testMatch holdem.limit.2p.reverse_blinds.game 1000 0 Alice Bob 12177 13177
